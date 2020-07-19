b86c8fef2ee740749d45025d4dd5988dd1f734fd
xedakini <xedakini@explicate.org>
2020-07-19 06:49:18 -0700

    improve (hopefully) the "--help" output

--- replicator/Params.py
+++ replicator/Params.py
@@ -14,32 +14,33 @@ def parse_args():
             raise argparse.ArgumentTypeError('value must be a positive number')
         return x
 
-    parser = argparse.ArgumentParser(description='http-replicator: a caching http proxy')
+    parser = argparse.ArgumentParser(description='http-replicator: a caching http proxy',
+                                     add_help=False)
     # yapf: disable
     parser.add_argument(
-        '-p', '--port', default=8080, type=port_number,
+        '--port', '-p', default=8080, type=port_number,
         help='listen on PORT for incoming connections (default=8080)')
     parser.add_argument(
-        '-b', '--bind', default='::1', metavar='ADDRESS',
+        '--bind', '-b', default='::1', metavar='ADDRESS',
         help='bind server to ADDRESS (default=::1)')
     parser.add_argument(
-        '-r', '--root', default=os.getcwd(),
-        help='set cache base directory to ROOT, default is the current directory')
+        '--root', '-r', default=os.getcwd(), metavar='ROOTDIR',
+        help='set cache base directory to ROOTDIR (default is the current directory)')
     parser.add_argument(
-        '-v', '--verbose', default=0, action='count',
-        help='show transaction activity; use twice for debugging info')
-    parser.add_argument(
-        '-t', '--timeout', default=15, type=positive_number,
+        '--timeout', '-t', default=15, type=positive_number,
         help='break connection after TIMEOUT seconds of inactivity (default=15)')
     parser.add_argument(
+        '--verbose', '-v', default=0, action='count',
+        help='show transaction activity; use twice for debugging')
+    parser.add_argument(
         '--flat', action='store_true',
-        help='flat mode; cache all files in ROOT directory (dangerous!)')
+        help='flat mode: cache all files in ROOTDIR (dangerous!)')
     parser.add_argument(
         '--static', action='store_true',
-        help='static mode; assume files never change')
+        help='static mode: assume files never change')
     parser.add_argument(
         '--offline', action='store_true',
-        help='offline mode; never initiate a network connection')
+        help='offline mode: never initiate a network connection')
     parser.add_argument(
         '--limit', default=0, type=float,
         help='cap download rate to LIMIT KiB/s')
@@ -49,6 +50,9 @@ def parse_args():
     parser.add_argument(
         '--pidfile', '--pid', metavar='PIDFILE',
         help='if --daemon is used, write pid of daemon to PIDFILE')
+    parser.add_argument(
+        '--help', '-h', action='help',
+        help='show this help message and exit')
     # yapf: enable
 
     global OPTS
